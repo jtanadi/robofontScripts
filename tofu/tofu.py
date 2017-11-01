b5e199a909b64d01442cc1c387a4f2308205ee65
jtanadi <mail@jesentanadi.com>
2017-10-31 23:33:35 -0400

    minor refactor

--- tofu/tofu.py
+++ tofu/tofu.py
@@ -3,16 +3,12 @@ import string
 
 class Tofu(object):
     def __init__(self):
-        self.inputText = ""
-        self.tofuText = ""
-        self.ucCheck = 0
-        self.lcCheck = 0
-        self.digitCheck = 0
-        self.punctCheck = 0
+        self.inputText, self.tofuText = "", ""
+        self.ucCheck, self.lcCheck, self.digitCheck, self.punctCheck = 0, 0, 0, 0
 
-        row = 10
         self.window = Window((500, 610), "Tofu")
 
+        row = 10
         self.window.inputTitle = TextBox((10, row, 100, 20),
                                          "Input:")
         self.window.inputText = TextEditor((10, row+25, -10, 200),
@@ -38,7 +34,7 @@ class Tofu(object):
 
         self.window.punctCheck = CheckBox((400, row+74, 100, 20),
                                           "No puncts.",
-                                          callback=self.punctCheckCallback)  
+                                          callback=self.punctCheckCallback)
 
         row += 90
         self.window.tofuButton = Button((200, row, 100, 30),
@@ -74,11 +70,11 @@ class Tofu(object):
         self.digitCheck = sender.get()
 
     def punctCheckCallback(self, sender):
-        self.punctCheck = sender.get()    
+        self.punctCheck = sender.get()
 
     def tofuButtonCallback(self, sender):
         outputText = self.inputText
-        noBueno = ""
+        noBueno = self.tofuText
 
         if self.ucCheck == 1:
             outputText = self.inputText.upper()
@@ -88,11 +84,11 @@ class Tofu(object):
 
         if self.digitCheck == 1:
             noBueno += string.digits
-        
+
         if self.punctCheck == 1:
             noBueno += string.punctuation
 
-        outputText = "".join(letter for letter in outputText if letter not in self.tofuText and letter not in noBueno)
+        outputText = "".join(letter for letter in outputText if letter not in noBueno)
 
         self.window.outputText.set(outputText)
 
