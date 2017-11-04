c2977bf3cedce89f4acdeda942c8f15a28ca5ea5
Jesen Tanadi <jesen@Jesens-MBP.home>
2017-11-03 22:40:13 -0400

    update & refactor

--- patternPreview/patternPreview.py
+++ patternPreview/patternPreview.py
@@ -2,62 +2,60 @@ from vanilla import *
 from mojo.events import addObserver, removeObserver
 from mojo.drawingTools import *
 
+f = CurrentFont()
 cG = CurrentGlyph()
+UPM = f.info.unitsPerEm
 
 class PatternPreview(object):
 
     def __init__(self):
-        self.heightRadio, self.heightInput, self.gridCheck = 0, 100, 0
-        self.height = CurrentFont().info.unitsPerEm
+        self.heightRadio, self.rowOnlyCheck = 0, 0
+        self.height = UPM
 
         self.buildUI()
 
     def buildUI(self):
         self.window = FloatingWindow((170, 160),
                                      closable=False)
+
         row = 5
         self.window.heightTitle = TextBox((10, row, -10, 17),
                                           "Cell Height:")
+
         row += 20
         self.window.heightRadio = RadioGroup((10, row, -10, 42),
                                              ["Use font UPM", "Use custom height"],
                                              callback=self.heightRadioCallback)
-
+        self.window.heightRadio.set(0)
 
         self.window.heightInput = EditText((30, row + 45, -10, 22),
-                                           text=self.heightInput,
+                                           text=self.height,
                                            callback=self.heightInputCallback)
+        self.window.heightInput.enable(False)
 
         row += 75
-        self.window.gridCheck = CheckBox((12, row, -10, 22),
-                                         "9-grid",
-                                         callback=self.gridCallback)
+        self.window.rowOnlyCheck = CheckBox((12, row, -10, 22),
+                                            "Single row only",
+                                            callback=self.rowOnlyCallback)
 
         row += 30
         self.window.closeButton = Button((10, row, -10, 20),
                                          "Close",
                                          callback=self.closeButtonCallback)
 
-        addObserver(self, "drawPattern", "drawBackground")
-        addObserver(self, "previewPattern", "drawPreview")
+        addObserver(self, "showPatternBackground", "drawBackground")
+        addObserver(self, "showPatternPreview", "drawPreview")
 
-        self.setDefaults()
         self.window.open()
-
-
-    def setDefaults(self):
-        self.window.heightRadio.set(0)
-        self.window.heightInput.enable(False)
 
     def heightRadioCallback(self, sender):
         self.heightRadio = sender.get()
 
         if self.heightRadio == 0:
-            self.height = CurrentFont().info.unitsPerEm
+            self.height = UPM
             self.window.heightInput.enable(False)
 
         elif self.heightRadio == 1:
-            self.height = self.heightInput
             self.window.heightInput.enable(True)
 
         cG.update()
@@ -66,64 +64,56 @@ class PatternPreview(object):
         self.window.heightInput.set(sender.get().lstrip("0"))
 
         try:
-            self.heightInput = int(sender.get())
-
+            self.height = int(sender.get())
         except ValueError:
-            self.window.heightInput.set(self.heightInput)
+            self.window.heightInput.set(self.height)
 
-        #self.drawPattern()
+        cG.update()
 
-    def gridCallback(self, sender):
-        self.gridCheck = sender.get()
-        #self.drawPattern()
+    def rowOnlyCallback(self, sender):
+        self.rowOnlyCheck = sender.get()
+        cG.update()
 
     def closeButtonCallback(self, sender):
         removeObserver(self, "drawBackground")
         removeObserver(self, "drawPreview")
         self.window.close()
 
-    def drawPattern(self, info):
-        g = info.get("glyph", "")
+    def showPatternBackground(self, info):
+        glyph = info.get("glyph", "")
+        fill(0, 0, 0, 0.5)
+        self.drawPattern(glyph)
 
-        if self.gridCheck == 1:
+    def showPatternPreview(self, info):
+        glyph = info.get("glyph", "")
+        fill(0)
+        self.drawPattern(glyph)
+
+    def drawPattern(self, glyph):
+        if self.rowOnlyCheck == 0:
             for col in range(-1, 2):
                 for row in range(-1, 2):
                     if (col or row) != 0:
                         save()
-                        fill(0, 0, 0, .5)
-                        translate(col * g.width, row * self.height)
-                        drawGlyph(g)
+                        translate(col * glyph.width, row * self.height)
+                        drawGlyph(glyph)
                         restore()
 
         else:
             for col in range(-1, 2):
                 if col != 0:
                     save()
-                    fill(0, 0, 0, .5)
-                    translate(col * g.width, 0)
-                    drawGlyph(g)
+                    translate(col * glyph.width, 0)
+                    drawGlyph(glyph)
                     restore()
 
-    def previewPattern(self, info):
-        gg = info.get("glyph", "")
-
-        if self.gridCheck == 1:
-            for col in range(-1, 2):
-                for row in range(-1, 2):
-                    if (col or row) != 0:
-                        save()
-                        fill(0, 0, 0, 1)
-                        translate(col * gg.width, row * self.height)
-                        drawGlyph(gg)
-                        restore()
-
-        else:
-            for col in range(-1, 2):
-                if col != 0:
-                    save()
-                    fill(0, 0, 0, 1)
-                    translate(col * gg.width, 0)
-                    drawGlyph(gg)
-                    restore()
 
 PatternPreview()
+
+"""
+---------------
+     TO DO
+---------------
++ BUG: Preview won't auto-refresh heights after switching from one glyph to another.
+       (User has to click on glyph window.)
+"""
