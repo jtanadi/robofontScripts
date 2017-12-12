3c74a85679a297aae926d5e9ca08d39a7144df3a
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-12-12 17:40:23 -0500

    make dummy layer

--- minMaxY/minMaxY.py
+++ minMaxY/minMaxY.py
@@ -3,9 +3,16 @@ f = CurrentFont()
 yPoints = []
 
 for g in f:
+    g.copyToLayer("tempCopy")
+    g.decompose()
+
     for contour in g.contours:
         for point in contour.points:
             yPoints.append(point.y)
+
+    g.flipLayers("tempCopy", "foreground")
+
+f.removeLayer("tempCopy")
 
 print "lowest y: {}".format(min(yPoints))
 print "highest y: {}".format(max(yPoints))
