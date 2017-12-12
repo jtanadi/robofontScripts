f5040398edf86639dbcc8a68440b1273061e3937
JesenTanadi <jesentanadi@JesenTanadi.local>
2017-12-12 16:04:46 -0500

    add steps to copy glyph to new layer, decompose, and restore

--- minMaxY/minMaxY.py
+++ minMaxY/minMaxY.py
@@ -1,11 +1,18 @@
 f = CurrentFont()
-
+g = CurrentGlyph()
 yPoints = []
 
 for g in f:
+    g.copyToLayer("tempCopy")
+    g.decompose()
+
     for contour in g.contours:
         for point in contour.points:
             yPoints.append(point.y)
+
+    g.flipLayers("tempCopy", "foreground")
+
+f.removeLayer("tempCopy")
 
 print "lowest y: {}".format(min(yPoints))
 print "highest y: {}".format(max(yPoints))
